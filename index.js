fef464ec4cee30b00fc4db78a503123b92dbb958
conorhastings <conor@socialtables.com>
2015-02-18 03:27:02 -0500

    use environmental variable in ~/.bash_profile to store github auth key, update readme, get latest commit info, get contents of readmes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Github = require('github-api');
 var app = koa();
 
 var github = new Github({
-  token: "place your token here",
+  token: process.env.GITHUB_KEY,
   auth: "oauth"
 });
 
@@ -13,20 +13,33 @@ var repo = github.getRepo("socialtables", "S3");
 app.use(function *(){
 
   repo.getTree('master?recursive=true', function(err, tree) {
-
+    
     if(err) {
       console.error('server error', err);
     }
 
     var i;
     for(i = 0; i < tree.length; i++) {
+      var readme = {}
       var path = tree[i].path;
+      var sha = tree[i].sha;
+      var url = tree[i].url;
+      readme.path = path;
+      readme.sha = sha;
+      readme.url = url;
       var index = path.indexOf('README');
       if(index >= 0) {
-        console.log(path);
+        repo.getCommits({path:path}, function(err,commits) {
+          //include author, author email, date of latest commit
+          console.log(commits[0].commit.committer);
+          //commit message
+          console.log(commits[0].commit.message);
+        })
+        repo.getBlob(sha, function(err, blob) {
+          console.log(blob);
+        })
       }
     }
-
   });
 
   this.body = 'Big Pink';
